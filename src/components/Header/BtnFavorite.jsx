07790423d4f92701de77ce1ae4ded77a46dd8ddd
Olena Yastrebtsova <uaalena202@gmail.com>
2023-12-30 17:06:47 +0200

    clicked function is added

--- src/components/Header/BtnFavorite.jsx
+++ src/components/Header/BtnFavorite.jsx
@@ -1,18 +1,34 @@
 ﻿import React from 'react';
 import heart from '../../assets/images/heart-primary.svg';
 import heartHover from '../../assets/images/heart-hover.svg';
-// import heartFill from '../../assets/images/heart-fill.svg';
+import heartClicked from '../../assets/images/heart-fill.svg';
 
 const BtnFavorite = () => {
     const[isHovered,setIsHovered] = React.useState(false);
-    const heartSrc = isHovered ? heartHover : heart;
+    const [isClicked, setIsClicked] = React.useState(false);
+   
+    let heartSrc = heart;
+    
+    if (isClicked) {
+        heartSrc = heartClicked;
+    } else if (isHovered) {
+        heartSrc = heartHover;
+    }
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+    };
   return (
     <div className=' ml-[120px]'>
+        <button
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={handleClick}
+        >
         <img  
         src={heartSrc} 
         alt=""
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)} />
+         />
+        </button>
     </div>
   )
 }
