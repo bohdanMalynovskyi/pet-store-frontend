da56328c2a048ae91ea1e4518d885d198b126bd6
Novik Kateryna <novik.katerina.96@gmail.com>
2024-01-15 05:05:27 +0200

    feat: Added footer desig. Add basic routing and default font

---
+++ src/style/icons/index.js
@@ -0,0 +1,6 @@
+export { LogoVertical } from './Logo-vertical';
+export { Instagram } from './Instagram';
+export { Facebook } from './facebook';
+export { Tiktok } from './Tiktok';
+export { Mastercard } from './Mastercard';
+export { Visa } from './Visa';
