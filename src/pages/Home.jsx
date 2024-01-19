4a6d03a798013f91cf0eec228ce7c55720ea762e
Olena Yastrebtsova <uaalena202@gmail.com>
2024-01-19 11:16:56 +0200

    ain page fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,11 @@
 ﻿import React from 'react';
-import Hero from '../components/Hero/Hero';
-import Deals from '../components/Deals/Deals';
-import New from '../components/New/New';
+
 
 
 const Home = () => {
   return (
     <div className=' flex flex-col'>
-      <Hero />
-      <div className=' container flex flex-col mx-auto max-w-[1260px] px-4'>
-        <Deals />
-        <New />
-      </div>
+      <p>Home</p>
     </div>
   )
 }
