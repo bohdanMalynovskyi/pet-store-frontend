d377d6d085c8eb9a1ab59ee6a701c8194a688481
Olena Yastrebtsova <uaalena202@gmail.com>
2024-01-19 19:40:36 +0200

    main page fixed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,28 +1,20 @@
 ﻿import React from 'react';
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 import Routes from '../../routes/Routers';
-import { useLocation } from 'react-router-dom';
 import ScrollToTop from "react-scroll-to-top";
 import Scrollup from "../../assets/images/scrollup.svg"
 
 
 
 const Layout = () => {
-    const location = useLocation();
-    const isHomePage = location.pathname === '/home';
     return (
         <div className=' font-ttnorms min-h-screen flex flex-col text-primary'>
-            <Header isHomePage={isHomePage} />
             <div className=' flex-auto'>
                 <Routes />
                 <ScrollToTop smooth
                 component={<img src={Scrollup} alt='scrollup'></img>}
                 />
             </div>
-            <Footer />
         </div>
-
     )
 }
 
