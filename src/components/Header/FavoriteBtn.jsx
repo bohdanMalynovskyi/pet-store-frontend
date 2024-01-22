b49339fcd3caade7b36ef03ccf3a9062533a53b9
Olena Yastrebtsova <uaalena202@gmail.com>
2024-01-22 11:48:47 +0200

    favorite btn changed onclick

--- src/components/Header/FavoriteBtn.jsx
+++ src/components/Header/FavoriteBtn.jsx
@@ -1,8 +1,19 @@
 ﻿import React, { useState, useEffect } from 'react';
-import { HeartIcon } from '@heroicons/react/24/outline';
 
 const Favorite = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+  const handleMouseDown = () => {
+    setIsClicked(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsClicked(false);
+  };
 
   useEffect(() => {
 
@@ -21,7 +32,27 @@ const Favorite = () => {
 
   return (
     <div className={` hover:text-hover ${navbarClasses}`}>
-      <HeartIcon/>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill={isClicked ? 'currentColor' : 'none'}
+        strokeWidth="1.5"
+        stroke="currentColor"
+        onClick={handleClick}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
+        style={{ cursor: 'pointer',
+        transition: 'fill 0.3s ease-in-out', }}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+        />
+      </svg>
+
+
     </div>
   );
 };
