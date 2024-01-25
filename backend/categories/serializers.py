2e15396ef50e038f17850737cf2cb883ef178509
Dima Saveliev <dp236955@gmail.com>
2024-01-25 18:33:31 +0200

    Serializers for categories
    Creating of serializers for "animal categories", "product categories" and their "subcategories".

---
+++ backend/categories/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from categories.models import AnimalCategory, ProductCategory, SubCategory
+
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = ['id', 'name']
+
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    subcategories = SubCategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ProductCategory
+        fields = ['id', 'name', 'subcategories']
+
+
+class AnimalCategorySerializer(serializers.ModelSerializer):
+    product_categories = ProductCategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = AnimalCategory
+        fields = ['id', 'name', 'product_categories']
