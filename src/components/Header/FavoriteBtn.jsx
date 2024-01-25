36805a2c3ae58344af5237fc0b89ba34889faad3
Olena Yastrebtsova <78073513+yastrb@users.noreply.github.com>
2024-01-25 10:56:55 +0200

    Update FavoriteBtn.jsx

--- src/components/Header/FavoriteBtn.jsx
+++ src/components/Header/FavoriteBtn.jsx
@@ -1,19 +1,7 @@
-﻿import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Favorite = () => {
   const [isMobile, setIsMobile] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-  };
-  const handleMouseDown = () => {
-    setIsClicked(true);
-  };
-
-  const handleMouseUp = () => {
-    setIsClicked(false);
-  };
 
   useEffect(() => {
 
@@ -33,29 +21,21 @@ const Favorite = () => {
   return (
     <div className={` hover:text-hover ${navbarClasses}`}>
       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill={isClicked ? 'currentColor' : 'none'}
-        strokeWidth="1.5"
-        stroke="currentColor"
-        onClick={handleClick}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseUp}
-        style={{ cursor: 'pointer',
-        transition: 'fill 0.3s ease-in-out', }}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-        />
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(0, 2)"
+        clipPath="url(#clip0_35_333)"
+          filter="url(#filter0_d_35_333)">
+          <path d="M18.2393 27C18.2393 27 5.23926 20 5.23926 11.75C5.23926 9.95979 5.95042 8.2429 7.21629 6.97703C8.48216 5.71116 10.199 5 11.9893 5C14.813 5 17.2318 6.53875 18.2393 9C19.2468 6.53875 21.6655 5 24.4893 5C26.2795 5 27.9964 5.71116 29.2622 6.97703C30.5281 8.2429 31.2393 9.95979 31.2393 11.75C31.2393 20 18.2393 27 18.2393 27Z"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round" />
+        </g>
       </svg>
-
-
     </div>
   );
 };
-
 export default Favorite
 
