077286edb1967353b9a3139b616cb3c4769b0138
Dima Saveliev <dp236955@gmail.com>
2024-01-25 16:29:46 +0200

    Models for categories
    Creating of models for "animal categories", "product categories" and their "subcategories". Also making migrations and registration in admin

--- backend/categories/admin.py
+++ backend/categories/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from categories.models import AnimalCategory, SubCategory, ProductCategory
+
+admin.site.register(AnimalCategory)
+admin.site.register(ProductCategory)
+admin.site.register(SubCategory)
