fd5bd3d5d64ca34e3e39059f066e78029a3f0008
Dima Saveliev <dp236955@gmail.com>
2024-01-26 15:47:57 +0200

    Adding API documentation
    I`ve added swagger API auto-documentation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',
 
-    'categories.apps.CategoriesConfig'
+    'categories.apps.CategoriesConfig',
 ]
 
 MIDDLEWARE = [
