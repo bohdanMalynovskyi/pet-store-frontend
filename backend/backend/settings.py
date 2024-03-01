54b68755dbe667f6504b220945cc98693b2ea11f
Dima Saveliev <dp236955@gmail.com>
2024-03-02 00:49:00 +0200

    Modifying .env and settings.py due to adding smtp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -185,7 +185,7 @@ DJOSER = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'groupforapumping@gmail.com'
-EMAIL_HOST_PASSWORD = 'vnhulqrlaorxjsjy'
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
