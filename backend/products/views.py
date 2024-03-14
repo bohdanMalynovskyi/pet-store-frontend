c380b251599f1e3834b570b8b00defa178e7333a
Dima Saveliev <dp236955@gmail.com>
2024-03-14 16:48:53 +0200

    Adding product filtering
    I`ve added product filtering by min and max price and by all types of categories(animal, product and subcategories)

--- backend/products/views.py
+++ backend/products/views.py
@@ -22,9 +22,33 @@ class ProductViewSet(ReadOnlyModelViewSet):
 
     def get_queryset(self):
         if self.action == 'list':
-            return Product.objects.all().order_by('id').prefetch_related('changeable_prices', 'images').annotate(
+            queryset = Product.objects.all().prefetch_related('changeable_prices', 'images').annotate(
                 discount_price=F('price') - (F('price') * F('discount') / 100))
-        return self.queryset
+
+            min_price = self.request.query_params.get('min_price')
+            max_price = self.request.query_params.get('max_price')
+            subcategory = self.request.query_params.get('subcategory')
+            animal_category = self.request.query_params.get('animal_category')
+            product_category = self.request.query_params.get('product_category')
+
+            if min_price is not None:
+                queryset = queryset.filter(discount_price__gte=min_price)
+
+            if max_price is not None:
+                queryset = queryset.filter(discount_price__lte=max_price)
+
+            if subcategory is not None:
+                queryset = queryset.filter(subcategory_id=subcategory)
+
+            if animal_category is not None:
+                queryset = queryset.filter(subcategory__product_category__animal_category_id=animal_category)
+
+            if product_category is not None:
+                queryset = queryset.filter(subcategory__product_category_id=product_category)
+
+            return queryset
+        else:
+            return self.queryset
 
     def get_ordering(self):
         ordering = self.request.query_params.get('ordering', None)
