3ce3600b654a7f3748c9150a0082e168a34ee84b
Dima Saveliev <dp236955@gmail.com>
2024-04-03 01:24:32 +0300

    Updating controllers
    i`ve updated logic.py funcs, views.py and tasks.py due to new serializers and db structure
    also I have deleted celery-tasks for deleting old featured and cart because now we have to store it

--- backend/users/logic.py
+++ backend/users/logic.py
@@ -24,7 +24,9 @@ def authorize_cart(view_func):
             except AttributeError:
                 return Response({'error': 'no token provided'}, status=status.HTTP_401_UNAUTHORIZED)
             try:
-                cart = Cart.objects.select_related('hash_code').values('id').get(hash_code__key=hash_code)
+                cart = Cart.objects.select_related('unregistered_user', 'unregistered_user__hash_code').values(
+                    'id').get(
+                    unregistered_user__hash_code__key=hash_code)
                 set_interact.delay(cart_id=cart['id'])  # set "last interact" field to now
                 return view_func(request, cart['id'], *args, **kwargs)
             except Cart.DoesNotExist:
@@ -49,8 +51,9 @@ def authorize_featured(view_func):
             except AttributeError:
                 return Response({'error': 'no token provided'}, status=status.HTTP_401_UNAUTHORIZED)
             try:
-                featured = FeaturedProducts.objects.select_related('hash_code').values('id').get(
-                    hash_code__key=hash_code)
+                featured = FeaturedProducts.objects.select_related('unregistered_user',
+                                                                   'unregistered_user__hash_code').values('id').get(
+                    unregistered_user__hash_code__key=hash_code)
                 set_interact.delay(featured_id=featured['id'])  # set "last interact" field to now
                 return view_func(request, featured['id'], *args, **kwargs)
             except FeaturedProducts.DoesNotExist:
@@ -61,7 +64,7 @@ def authorize_featured(view_func):
 
 def get_cart(cart_id):
     """This func was made to get rid of repeating cart getting query"""
-    cart = Cart.objects.select_related('hash_code').prefetch_related(
+    cart = Cart.objects.select_related('unregistered_user', 'unregistered_user__hash_code').prefetch_related(
         Prefetch('cart_items', queryset=CartItem.objects.select_related('product').prefetch_related(
             'product__changeable_prices', 'product__images')
                  )
@@ -71,7 +74,7 @@ def get_cart(cart_id):
 
 def get_featured(featured_id):
     """This func was made to get rid of repeating featured getting query"""
-    featured = FeaturedProducts.objects.select_related('hash_code').prefetch_related(
+    featured = FeaturedProducts.objects.select_related('unregistered_user', 'unregistered_user__hash_code').prefetch_related(
         Prefetch('featured_items', queryset=FeaturedItem.objects.select_related('product').prefetch_related(
             'product__changeable_prices', 'product__images')
                  )
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from users import views
