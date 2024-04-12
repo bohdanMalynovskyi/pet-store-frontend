53089c14b2384ba756ad380bf47887969c644f82
Dima Saveliev <dp236955@gmail.com>
2024-04-12 11:57:27 +0300

    Updating swagger documentation due to updated logic

--- backend/orders/docs.py
+++ backend/orders/docs.py
@@ -6,6 +6,10 @@ create_order_body = openapi.Schema(
         'payment_type': openapi.Schema(type=openapi.TYPE_STRING, enum=["cash", "online"]),
         'warehouse_index': openapi.Schema(type=openapi.TYPE_STRING),
         'city_ref': openapi.Schema(type=openapi.TYPE_STRING),
+        'first_name': openapi.Schema(type=openapi.TYPE_STRING, description='required if not authenticated'),
+        'second_name': openapi.Schema(type=openapi.TYPE_STRING),
+        'last_name': openapi.Schema(type=openapi.TYPE_STRING, description='required if not authenticated'),
+        'phone': openapi.Schema(type=openapi.TYPE_STRING, description='required if not authenticated'),
     },
     required=['payment_type', 'warehouse_index', 'city_ref']
 )
